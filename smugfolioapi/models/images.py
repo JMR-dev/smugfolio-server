0cf8781c937baa984886932cb03b0f52ebb6020a
bytesparallel <jason.ross841@gmail.com>
2022-03-15 14:41:34 -0500

    adding corrected fixtures

--- smugfolioapi/models/images.py
+++ smugfolioapi/models/images.py
@@ -5,4 +5,7 @@ class Images(models.Model):
     file_path = models.CharField(max_length=1024)
     album_id = models.IntegerField(default=0)
     smug_user_id = models.ForeignKey("Smug_Users", on_delete=models.CASCADE)
-    upload_date = models.DateTimeField()
+    upload_date = models.DateTimeField(auto_now_add=True)
+    
+    # I believe that auto_now_add is the correct syntax. If I'm understanding this correctly, it only creates a timestamp when the object is first created.
+    # https://www.geeksforgeeks.org/datetimefield-django-models/
